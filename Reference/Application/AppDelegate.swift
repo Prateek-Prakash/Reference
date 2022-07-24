d02b473416b4edf8c92022ae361b4766e70ee39d
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2022-07-24 11:04:38 -0400

    Update Folder Structure

---
+++ Reference/Application/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Reference
+//
+//  Created by Prateek Prakash on 7/24/22.
+//
+
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
